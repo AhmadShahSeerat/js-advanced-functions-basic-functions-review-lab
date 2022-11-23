ebf476d68299d4d0b0595a87cdcd64a600029274
AhmadShah <ah.seerat@gmail.com>
2022-11-23 07:22:36 -0500

    commit tree e27c0849b2de3ea5b24a8872777ec05c846fa8ba

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-// Your code here
+// // Your code here
+// function saturdayFun(fun= "roller-skate"){
+//     console.log(`"This Saturday, I want to ${fun}`)
+// };
+
+// let mondayWork = function(a){
+//     console.log(`This Monday, I will ${a}`);
+// };
+
+
+// let wrapAdjective = function(style="*") {
+//     return function(adjective="special") {
+//       return `You are ${style}${adjective}${style}!`
+//     }
+//   }
+  
+//   const Calculator = {
+//     add: function(a,b) {
+//       return a + b;
+//     },
+//     subtract: function(a,b) {
+//       return a - b
+//     },
+//     multiply: function(a,b) {
+//       return a * b
+//     },
+//     divide: function(a,b) {
+//       return a / b;
+//     }
+//   }
+  
+//   let actionApplyer = function(start, ray) {
+//     let a = start
+  
+//     for (let i = 0; i < ray.length; i++ ){
+//       a = ray[i](a)
+//     }
+  
+//     return a
+//   }
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+  
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+  
+  const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+  
+  let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
